5e1d63ed4c80c1f7f79f34c7eda71ea0344ccbcf
Marchael Acode <m.acode@outlook.com>
2021-10-04 00:24:17 -0700

    Created the mapview app bar and added a FAB for dropping a pin on the map.

--- app/src/main/java/com/example/geocachingandroidcodingexercise/geocachingMap/MapViewScreen.kt
+++ app/src/main/java/com/example/geocachingandroidcodingexercise/geocachingMap/MapViewScreen.kt
@@ -1,38 +1,67 @@
 package com.example.geocachingandroidcodingexercise.geocachingMap
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings
-import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.CenterFocusStrong
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
-@ExperimentalPermissionsApi
+
 @Composable
 fun MapViewScreen(
     navController: NavController,
     viewModel: MapViewViewModel = hiltViewModel()
 ) {
-    Text(text = "Map View Screen")
+    Scaffold(
+        topBar = { MapViewAppBar() },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = { FloatingActionButton(
+            onClick = { /*TODO*/ },
+        ) {
+            Image(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "Add Icon",
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+        }
+        }
+    ) {
+        Column() {
+            
+        }
+    }
+}
+
+@Composable
+fun MapViewAppBar() {
+    TopAppBar(
+        elevation = 4.dp,
+        title = {
+            Text(text = "Geocaching")
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Filled.Navigation, contentDescription = "Navigate Icon")
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Filled.CenterFocusWeak, contentDescription = "Center Map Icon")
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Filled.Calculate, contentDescription = "Calculate Distance Icon")
+            }
+        }
+    )
 }
 
 

0b23e826a98293e8b66b510e92d2140bfe65f702
Marchael Acode <m.acode@outlook.com>
2021-10-04 12:19:59 -0700

    Map is viewable for the user and users current location is shown on the map. Currently working on updating users location and shown on the map.

--- app/src/main/java/com/example/geocachingandroidcodingexercise/geocachingMap/MapViewScreen.kt
+++ app/src/main/java/com/example/geocachingandroidcodingexercise/geocachingMap/MapViewScreen.kt
@@ -1,20 +1,38 @@
 package com.example.geocachingandroidcodingexercise.geocachingMap
 
 
+import android.os.Bundle
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.CenterFocusStrong
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.MapView
+import com.google.android.libraries.maps.model.LatLng
+import com.google.android.libraries.maps.model.MarkerOptions
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.utils.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.IllegalStateException
 
 
 @Composable
@@ -24,7 +42,7 @@ fun MapViewScreen(
 ) {
     Scaffold(
         topBar = { MapViewAppBar() },
-        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButtonPosition = FabPosition.Center,
         floatingActionButton = { FloatingActionButton(
             onClick = { /*TODO*/ },
         ) {
@@ -36,9 +54,9 @@ fun MapViewScreen(
         }
         }
     ) {
-        Column() {
-            
-        }
+        viewModel.getLocationPermission(LocalContext.current)
+        val destination = LatLng(viewModel.userCurrentLat.value, viewModel.userCurrentLng.value)
+        LoadMapView(viewModel = viewModel, destination = destination)
     }
 }
 
@@ -62,6 +80,65 @@ fun MapViewAppBar() {
             }
         }
     )
+}
+
+@Composable
+fun LoadMapView(viewModel: MapViewViewModel, destination: LatLng) {
+    val mapView = rememberMapViewLifecycle()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        AndroidView( {mapView} ) {
+            CoroutineScope(Dispatchers.Main).launch {
+                val map = mapView.awaitMap()
+                map.uiSettings.isZoomControlsEnabled = true
+                
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 18f))
+
+                val userMarker = MarkerOptions()
+                    .title("User Location")
+                    .position(destination)
+                map.addMarker(userMarker)
+            }
+        }
+    }
+}
+
+@Composable
+fun rememberMapViewLifecycle(): MapView {
+    val context = LocalContext.current
+
+    val mapView = remember {
+        MapView(context).apply { id = R.id.map_frame }
+    }
+    val lifeCycleObserver = rememberMapLifecycleObserver(mapView = mapView)
+    val lifeCycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(lifeCycle) {
+        lifeCycle.addObserver(lifeCycleObserver)
+        onDispose {
+            lifeCycle.removeObserver(lifeCycleObserver)
+        }
+    }
+
+    return mapView
+}
+
+@Composable
+fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver = remember(mapView) {
+    LifecycleEventObserver { _, event ->
+        when (event) {
+            Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+            Lifecycle.Event.ON_START -> mapView.onStart()
+            Lifecycle.Event.ON_RESUME -> mapView.onResume()
+            Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+            Lifecycle.Event.ON_STOP -> mapView.onStop()
+            Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+            else -> throw IllegalStateException()
+        }
+    }
 }
 
 
